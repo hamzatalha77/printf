fb64f61564939039d06e17df65f5974d6fffb279
hamzatalha77 <hamza_ta7@hotmail.com>
2023-08-22 05:08:46 -0700

    do c function with betty

--- function_s.c
+++ function_s.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
-/************************* PRINT CHAR *************************/
-
 /**
- * print_char - Prints a char
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: Width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of chars printed
+ * print_char - Prints a character.
+ *
+ * This function is responsible for printing a single character specified by the
+ * argument list. It takes several parameters:
+ * - @types: List of arguments (not explicitly mentioned in the comment).
+ * - @buffer: Buffer array used for printing.
+ * - @flags: A variable that calculates active flags.
+ * - @width: Width specification.
+ * - @precision: Precision specification.
+ * - @size: Size specifier (not explicitly mentioned in the comment).
+ *
+ * Return: The number of characters printed.
  */
+
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
